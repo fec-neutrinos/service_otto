9c4e61b5900b96cb81e695a2df6962ce28114944
Otto Sanchez <otto.sancheza@gmail.com>
2019-10-12 13:20:36 -0500

    updated cors to get the proxy server working

--- server/index.js
+++ server/index.js
@@ -2,7 +2,11 @@ const express = require('express');
 const app = express();
 const routes = require('./routes');
 const port = 3010;
+const cors = require('cors');
+const morgan = require('morgan');
 
+app.use(cors());
+app.use(morgan('dev'));
 app.use(express.static('public'));
 app.use('/', routes);
 
